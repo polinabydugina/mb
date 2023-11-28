4d82abf585d55bddbc9db47d86984249155547f8
polinabydugina <xch.victor@mail.ru>
2023-11-28 23:39:22 +0300

    ляляля

--- морской бой/Program.cs
+++ морской бой/Program.cs
@@ -28,17 +28,39 @@ while (true)
             Console.WriteLine();
         }
     }
-    Console.WriteLine("введите индексы однопалубного коробля");
-    int x = Convert.ToInt32(Console.ReadLine());
-    int y = Convert.ToInt32(Console.ReadLine());
-
-    if (mb[x - 1, y] == " # " || mb[x, y - 1] == " # " || mb[x + 1, y] == " # " || mb[x, y + 1] == " # " || mb[x - 1, y - 1] == " # " || mb[x + 1, y + 1] == " # " || mb[x + 1, y - 1] == " # ")
+    int t = 0;
+    while (t < 4)
     {
-        Console.WriteLine("Низя!");
-    }
-    else
-    {
-        mb[x, y] = " # ";
+        Console.WriteLine("введите индексы однопалубного корабля");
+        int x = Convert.ToInt32(Console.ReadLine());
+        int y = Convert.ToInt32(Console.ReadLine());
+  
+        if (mb[x - 1, y] == " # " || mb[x, y - 1] == " # " || mb[x + 1, y] == " # " || mb[x, y + 1] == " # " || mb[x - 1, y - 1] == " # " || mb[x + 1, y + 1] == " # " || mb[x + 1, y - 1] == " # ")
+        {
+            Console.WriteLine("Низя!");
+        }
+        else
+        {
+            mb[x, y] = " # ";
+            t++;
+            Console.Clear();
+        }
         vivod(mb);
+    }
+
+    int n = 0;
+    while (t < 3)
+    {
+        Console.WriteLine("введите индексы первой клетки двупалубного корабля");
+        int x = Convert.ToInt32(Console.ReadLine());
+        int y = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("введите индексы второй клетки двупалубного корабля");
+        int p = Convert.ToInt32(Console.ReadLine());
+        int l = Convert.ToInt32(Console.ReadLine());
+        if ()
+        {
+
+        }
+        n++;
     }
 }
